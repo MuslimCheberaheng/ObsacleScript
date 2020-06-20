ead61a6f64cbee038f61d727fd628bfdca5419fb
Muslim Cheberaheng <66585845+MuslimCheberaheng@users.noreply.github.com>
2020-06-20 18:27:05 +0700

    Update Spawner.cs

--- Spawner.cs
+++ Spawner.cs
@@ -9,20 +9,14 @@ public class Spawner : MonoBehaviour
     private float TBS; //time between spawn
     public float startTBS; //start time between spawn
     public float decreaseTime;
-    public float minTime = 0.65f;
-
-    void Start()
-    {
-        
-    }
-
+    public float minTime = 0.65f;    
     
     void Update()
     {
         if (TBS <= 0)
         {
             int rand = Random.Range(0, obstaclePatterns.Length);
-            Instantiate(obstaclePatterns[rand], transform.position, Quaternion.identity);
+            Instantiate(obstaclePatterns[rand], transform.position, Quaternion.identity); //random instantiate
             TBS = startTBS;
             if (startTBS > minTime)
             {
